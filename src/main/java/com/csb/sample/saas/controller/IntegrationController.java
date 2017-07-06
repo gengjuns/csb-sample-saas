8cf1c104c9a56fa711e7ec94f8e1f79ec0ef1f0d
gengjun <gengjunlover@gmail.com>
2017-07-06 11:19:39 +0800

    Update IntegrationController.java

--- src/main/java/com/csb/sample/saas/controller/IntegrationController.java
+++ src/main/java/com/csb/sample/saas/controller/IntegrationController.java
@@ -30,8 +30,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * 功能： 演示供应商系统如何将系统集成到信息消费大卖场<br>
  * 版本： 1.0<br>
  * 日期： 2015-11<br>
- * 作者： 信息消费大卖场团队<br>
- * 版权： 信息消费大卖场<br>
  * 说明： 以下代码只是为了方便供应商集成测试而提供的样例代码，供应商可根据自己需要，按照技术集成文档编写。该代码仅供参考。<br>
  */
 @Controller
@@ -41,10 +39,10 @@ public class IntegrationController {
     private static Logger logger = LoggerFactory.getLogger(IntegrationController.class);
 
     /**
-     * 该方法处理从信息消费大卖场传递过来的请求，包括：订购，更新，取消，用户分配，取消用户分配
+     * 该方法处理从创客超市平台传递过来的请求，包括：订购，更新，取消，用户分配，取消用户分配
      * 
      * @param url
-     *            信息消费大卖场平台数据接口地址
+     *            创客超市平台数据接口地址
      * @return 处理结果信息
      */
     @RequestMapping(value = "/integration", method = { RequestMethod.GET, RequestMethod.POST })
